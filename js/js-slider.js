c075467cb136a2c9bf1eacb5aecb3f5425b17fb2
Guillaume Briday <guillaumebriday@gmail.com>
2016-11-21 00:15:10 +0100

    Correction de l'indentation

--- js/js-slider.js
+++ js/js-slider.js
@@ -1,35 +1,30 @@
 (function($){
-
-    $.fn.slider=function(options){
-
-    	var defauts = {
-			'rollback': true,
+    $.fn.slider = function (options) {
+        var defauts = {
+            'rollback': true,
             'duration' : 400,
             'width' : '50%'
-        };  
-           
-        var params = $.extend(defauts, options);
+        };
 
-       return this.each(function(){
+        var params = $.extend (defauts, options);
 
-	       	var container = $(this);
-	       	var lastImgContainer = container.find(".js-slider-last-container");
+        return this.each (function() {
+            var container = $(this);
+            var lastImgContainer = container.find(".js-slider-last-container");
 
-            container.mousemove(function(e){
-        		$(lastImgContainer).css({
-        			"width" : container.width() - (e.pageX - container.offset().left) // container's width - mouse's position from left in the container 
-        		});
-	    	});
+            container.mousemove( function(e) {
+                $( lastImgContainer ).css({
+                    "width" : container.width() - (e.pageX - container.offset().left) // container's width - mouse's position from left in the container
+                });
+            });
 
-
-            if(params.rollback){
-            	container.mouseleave(function(){
-		        	lastImgContainer.animate({
-		        		"width" : params.width
-		        	}, params.duration);
-		    	});
+            if (params.rollback) {
+                container.mouseleave( function() {
+                    lastImgContainer.animate({
+                        "width" : params.width
+                    }, params.duration);
+                });
             }
-
-       });
+        });
     };
 })(jQuery);
